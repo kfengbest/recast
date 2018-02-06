f3cbf1409c8bb19eac74b4dabb03f649006fa19a
Weiliang Qian <weiliang.qian@autodesk.com>
2018-02-06 14:20:39 +0800

    add load test script

---
+++ k8s/siege.sh
@@ -0,0 +1,6 @@
+siege -c10  -t10m -i -t10S -f siege.url
+siege -c30  -t10m -i -t20S -f siege.url
+siege -c50  -t10m -i -t10M -f siege.url
+#siege -c80  -t10m -i -t10S -f siege.url
+#siege -c100 -t10m -i -t1M -f siege.url
+#siege -c120 -t10m -i -t10M  -f siege.url
